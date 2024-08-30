2cd733b3c4f953b5876bbd58c970d2032ac5b220
victorflll <victorluan.lobo@gmail.com>
2024-08-30 00:37:14 -0300

    feat: add annotation for email validation

--- src/domain/models/member/create.member.dto.ts
+++ src/domain/models/member/create.member.dto.ts
@@ -1,6 +1,8 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail} from "class-validator";
 
 export class CreateMemberDto {
+    @IsEmail({}, { message: 'Invalid email format' })
     @ApiProperty({ example: 'joao@email.com' })
     email: string;
     @ApiProperty({ example: false })
--- src/domain/models/member/update.member.dto.ts
+++ src/domain/models/member/update.member.dto.ts
@@ -1,6 +1,8 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail} from "class-validator";
 
 export class UpdateMemberDto {
+    @IsEmail({}, { message: 'Invalid email format' })
     @ApiProperty({ example: 'João da Silva' })
     name: string;
 }
