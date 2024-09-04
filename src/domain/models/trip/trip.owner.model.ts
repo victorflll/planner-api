eaaef4f055401c46eb6eb4ec55205d174cb82302
victorflll <victorluan.lobo@gmail.com>
2024-09-04 13:49:14 -0300

    feat: add dto validation using class-validator

--- src/domain/models/trip/trip.owner.model.ts
+++ src/domain/models/trip/trip.owner.model.ts
@@ -1,8 +1,13 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail, IsNotEmpty, IsString} from "class-validator";
 
 export class TripOwner {
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty({example: 'Maria de Souza'})
     name: string;
+    @IsEmail()
+    @IsNotEmpty()
     @ApiProperty({example: 'maria@email.com'})
     email: string;
 }
